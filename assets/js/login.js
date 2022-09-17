e9113035e7315a1d3ec43e5f77abbc274633c2a5
whyisshen <71068230@qq.com>
2022-09-17 14:36:25 +0800

    完成了登录和注册的开发

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function () {
+    // 点击'去注册账号'的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击'去登录'的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    // 从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    // 通过form.verify()函数自定义校验规则
+    form.verify({
+        // 自定义校验规则pwd
+        pwd: [/^[\S]{6,12}$/, '密码必须是6到12位,且不能出现空格'],
+        // 两次密码一致的规则
+        repwd: function (value,) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致!'
+            }
+        }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=username').val(),
+            password: $('#form_reg [name=password').val()
+        }
+        $.post('/api/reguser', data,
+            function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功');
+                // 模拟人的点击行为
+                $('#link_login').click()
+            })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败!')
+                }
+                layer.msg('登录成功!')
+                localStorage.setItem('token',res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+})
