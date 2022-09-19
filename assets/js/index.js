93b76c5d4e3e4ffb50c7105fb59d0c3edf3c6a88
whyisshen <71068230@qq.com>
2022-09-19 16:56:32 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,6 @@ $.ajax({
     method:'GET',
     url:"/my/userinfo",
     success: function(res) {
-        console.log(res);
         if(res.status !== 0) {
             return layui.layer.msg('获取用户信息失败!')
         }
